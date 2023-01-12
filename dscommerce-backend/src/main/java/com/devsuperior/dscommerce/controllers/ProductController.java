a0e33aa4724a723c9fb0700c42bb94680b226245
rodrigo.baggio.si@gmail.com <Baggio25>
2023-01-12 17:53:09 -0300

    Criando DTO e estruturando camadas - Aula 03-11

---
+++ dscommerce-backend/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dscommerce.controllers;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findAll(@PathVariable(name = "id" ) Long id) {
+        ProductDTO productDTO = productService.findById(id);
+        return productDTO;
+    }
+
+}
