a35f8afee410b6dc0adb70797835aeb2219068a6
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2023-07-03 17:07:12 -0300

    First axios test

--- frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,27 +1,35 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import * as productService from "../../../services/product-service";
+import axios from "axios";
 
+import * as productService from "../../../services/product-service";
 import { ButtomInverse } from "../../../components/ButtonInverse";
 import { ButtomPrimary } from "../../../components/ButtonPrimary";
 import { ProductDetailsCard } from "../../../components/ProductDetailsCard";
+import { ProductDTO } from "../../../models/product";
 
 import "./styles.css";
 
-
 export function ProductDetails() {
+  const [product, setProduct] = useState<ProductDTO>();
   const params = useParams();
-  const product = productService.findById(Number(params.productId));
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/products/1").then((response) => {
+      console.log(response.data);
+      setProduct(response.data);
+    });
+
+  }, []);
 
   return (
     <main>
       <section className="dsc-container">
-        { product && 
-          <ProductDetailsCard product={product} />
-        }
+        {product && <ProductDetailsCard product={product} />}
 
         <div className="dsc-btn-page-container">
           <ButtomPrimary text="Comprar" />
-          <ButtomInverse text="Início" url="/"/>
+          <ButtomInverse text="Início" url="/" />
         </div>
       </section>
     </main>
