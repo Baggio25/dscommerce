c8055a8d5969824b9c610d0bae235c1a64639791
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2023-07-03 09:21:40 -0300

    CatalogCard Props

--- frontend/src/components/CatalogCard/index.tsx
+++ frontend/src/components/CatalogCard/index.tsx
@@ -1,20 +1,24 @@
 
-import ProductImage from "../../assets/computer.png";
+import { ProductDTO } from "../../models/product";
 
 import "./styles.css";
 
-export function CatalogCard() {
+type CatalogCardProps = {
+  product: ProductDTO;
+}
+
+export function CatalogCard( { product } : CatalogCardProps) {
   return (
     <div className="dsc-card">
       <div className="dsc-catalog-card-top dsc-line-bottom">
-        <img src={ProductImage} alt="computer" />
+        <img src={product.imgUrl} alt={product.name} />
       </div>
       <div className="dsc-catalog-card-bottom">
         <h3 className="dsc-product-price dsc-product-price-20">
-          R$ 5.000,00
+          R$ {product.price}
         </h3>
         <h4 className="dsc-product-name dsc-product-name-14">
-          Nobebook DELL Inspiron 15,6`
+          {product.name}
         </h4>
       </div>
     </div>
