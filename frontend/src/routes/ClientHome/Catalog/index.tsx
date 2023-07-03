e2085bc3f6fe3f43c0ad8085f55e6c8881ad2ccf
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2023-07-03 10:12:07 -0300

    Adding react router dom

---
+++ frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -0,0 +1,56 @@
+import { ButtonNextPage } from "../../../components/ButtonNextPage";
+import { CatalogCard } from "../../../components/CatalogCard";
+import { SearchBar } from "../../../components/SearchBar";
+import { ProductDTO } from "../../../models/product";
+
+import "./styles.css";
+
+const product: ProductDTO = {
+  id: 1,
+  name: "Smartv LG 55 polegadas",
+  imgUrl:
+    "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
+  price: 3568.99,
+  description: "",
+  categories: [],
+};
+
+export function Catalog() {
+  return (
+    <main>
+      <section className="dsc-container">
+        <SearchBar />
+
+        <div className="dsc-catalog-cards dsc-mb-20 dsc-mt-20">
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+        </div>
+
+        <ButtonNextPage />
+      </section>
+    </main>
+  );
+}
